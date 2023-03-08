f7eae585d83839c0685f069838c9dbdc0db1dea6
sieunju <j.sieun73@gmail.com>
2023-03-08 20:51:18 +0900

    효도프로젝트 서버소스 첫 업로드

---
+++ src/main/java/com/academy/sosu/SosuApplication.java
@@ -0,0 +1,13 @@
+package com.academy.sosu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/sosu/SosuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.sosu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
